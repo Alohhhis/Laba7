b37b7ba9317208d40422030707e3c703a79d497a
Alohhhis <D_Alyona35@mail.ru>
2023-06-16 01:06:44 +0300

    User's info reading

--- client/src/main/kotlin/utils/UserCreatorReader.kt
+++ client/src/main/kotlin/utils/UserCreatorReader.kt
@@ -1,17 +1,24 @@
 package utils
 
+import data.User
 import java.util.Scanner
 
 class UserCreatorReader {
     private val scanner = Scanner(System.`in`)
     private val hashMan = HashingPassword()
-    private fun readUsername():String{
+    private fun readUsername(): String {
         println("Введите имя пользователя: ")
         return scanner.nextLine()
     }
 
-    private fun readUserPassword(): String{
+    private fun readUserPassword(): String {
         println("Введите пароль: ")
         return scanner.nextLine().toString().let { hashMan.hashPassword(it) }
+    }
+
+    fun readUserInfo(): User {
+        val username = readUsername()
+        val password = readUserPassword()
+        return User(username, password)
     }
 }
